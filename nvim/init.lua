c168ddf1480e5d00acb079b5866054dc509ae4c2
arash <shafieearash2000@yahoo.com>
2023-12-12 19:04:14 +0330

    nvim config:   - add file explorer   - remove unused treesitter config   - fix mapleader (should set before lazy.nvim setup)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,9 @@
 require('plugins.lazy')
 require('plugins.telescope')
 require('plugins.treesitter')
--- require('plugins.treesitter-ecovim')
+require('plugins.file-explorer')
+
 require('lsp.setup')
+
 require('options')
 require('remap')
